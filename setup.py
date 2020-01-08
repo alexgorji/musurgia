6560eeea658e9530d83b9eb83d073fb92dd71a80
Alex Gorji <aligorji@hotmail.com>
2020-01-08 09:59:28 +0100

    correct version 1.1.0 not 1.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="musurgia",
-    version="1.2.0",
+    version="1.1.0",
     author="Alex Gorji",
     author_email="aligorji@hotmail.com",
     description="tools for algorithmic composition",
