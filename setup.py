7a276dd736de2cc1cfe781b55ea94f145dc5e467
Alex Gorji <aligorji@hotmail.com>
2020-03-18 13:59:26 +0100

    wip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="musurgia",
-    version="1.10.5",
+    version="1.10.6",
     author="Alex Gorji",
     author_email="aligorji@hotmail.com",
     description="tools for algorithmic composition",
