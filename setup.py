a48d80c7991dae51428104aeac53a7b19c033b5a
Alex Gorji <aligorji@hotmail.com>
2020-04-07 13:09:49 +0200

    setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     url="https://github.com/alexgorji/musurgia.git",
     packages=setuptools.find_packages(),
     install_requires=['quicktions==1.9',
-                      'musicscore==1.2.16',
+                      'musicscore==1.3.1',
                       'prettytable==0.7.2',
                       'fpdf2 == 2.0.3',
                       'diff-pdf-visually == 1.4.1'
