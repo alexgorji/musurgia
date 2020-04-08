88894ab03bb1a4cb50fba08de802fa93defb0606
Alex Gorji <aligorji@hotmail.com>
2020-04-08 12:48:12 +0200

    issue 23 tests

--- tests/fractalmusic/deepcopy/test_fm_deepcopy.py
+++ tests/fractalmusic/deepcopy/test_fm_deepcopy.py
@@ -1,11 +1,17 @@
-from unittest import TestCase
+import os
 
+from musicscore.musictree.treescoretimewise import TreeScoreTimewise
+
+from musurgia.agunittest import AGTestCase
 from musurgia.fractaltree.fractalmusic import FractalMusic
 
+path = str(os.path.abspath(__file__).split('.')[0])
 
-class Test(TestCase):
+
+class Test(AGTestCase):
     def setUp(self) -> None:
-        fm = FractalMusic(value=10, reading_direction='vertical')
+        self.score = TreeScoreTimewise()
+        fm = FractalMusic(tempo=60, quarter_duration=10, reading_direction='vertical')
         fm.add_layer()
         fm.add_layer()
         self.fm = fm.get_children()[1]
@@ -36,3 +42,17 @@ class Test(TestCase):
 
     def test_2(self):
         self.assertNotEqual(self.deep_copied.__name__, self.fm.__name__)
+
+    def test_3(self):
+        for leaf in self.fm.traverse_leaves():
+            leaf.chord.add_words(leaf.fractal_order)
+
+        copied = self.fm.__deepcopy__()
+        copied.get_simple_format().to_stream_voice().add_to_score(self.score)
+        xml_path = path + '_test_3.xml'
+        self.score.write(xml_path)
+        expected_path = path + '_test_3_expected.xml'
+        expected_score = TreeScoreTimewise()
+        self.fm.get_simple_format().to_stream_voice().add_to_score(expected_score)
+        expected_score.write(expected_path)
+        self.assertCompareFiles(xml_path, expected_path)
