ec417d072922f09cc7ddae7fd9cb477b8b855a2d
Alex Gorji <aligorji@hotmail.com>
2020-04-25 22:30:04 +0200

    gearwheels new structure (Wheel, Position, etc.)

--- musurgia/gearwheels.py
+++ musurgia/gearwheels.py
@@ -1,12 +1,40 @@
 from musicscore.basic_functions import lcm, xToD
 
 
+class Position:
+    def __init__(self, value, wheel):
+        self._value = None
+        self._wheel = None
+        self.value = value
+        self.wheel = wheel
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        self._value = val
+
+    @property
+    def wheel(self):
+        return self._wheel
+
+    @wheel.setter
+    def wheel(self, val):
+        if not isinstance(val, Wheel):
+            raise TypeError('wheel.value must be of type Wheel not{}'.format(type(val)))
+        self._wheel = val
+
+
 class Wheel:
     def __init__(self, size, start=0):
         self._size = None
         self._start = None
+        self._number_of_cycles = None
         self.size = size
         self.start = start
+        self.index = None
 
     @property
     def size(self):
@@ -15,7 +43,7 @@ class Wheel:
     @size.setter
     def size(self, val):
         if not isinstance(val, int):
-            raise TypeError('size.value must be of type int not{}'.format(type(val)))
+            raise TypeError(f'size.value must be of type int not{type(val)}')
         self._size = val
 
     @property
@@ -26,67 +54,64 @@ class Wheel:
     def start(self, val):
         self._start = val
 
-    def get_positions(self, number_of_cycles):
-        return list(range(self.start, number_of_cycles * self.size + self.start, self.size))
+    def get_positions(self, number_of_cycles=None):
+        if not number_of_cycles:
+            number_of_cycles = self._number_of_cycles
+        if not number_of_cycles or not isinstance(number_of_cycles, int) or number_of_cycles < 0:
+            raise AttributeError(f'number_of_cycles must be a positive non zero int not {number_of_cycles}')
+        position_values = list(range(self.start, number_of_cycles * self.size + self.start, self.size))
+        return [Position(value, self) for value in position_values]
+
+    def get_position_values(self):
+        position_values = [p.value for p in self.get_positions()]
+        return list(dict.fromkeys(position_values))
 
 
-class GearWheels(object):
-    def __init__(self, gear_sizes, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._gear_sizes = None
-        self._cycles = None
+class GearWheels:
+    def __init__(self, wheels):
+        self._wheels = None
+        self._positions = None
         self._iterator = None
 
-        self.gear_sizes = gear_sizes
+        self.wheels = wheels
 
-    # //private methods
-    @staticmethod
-    def _check_input(value):
-        if not isinstance(value, list) or len(value) < 2:
-            raise ValueError('list_of_wheels should be a list with a minimum length of 2!')
-        else:
-            return True
-
-    def _set_cycles(self):
-        _cycles = []
+    def _set_positions(self):
+        _positions = []
         _lcm = lcm(self.gear_sizes)
-        for size in self.gear_sizes:
-            number_of_cycles = (_lcm // size) + 1
-            _cycles.append(range(0, number_of_cycles * size, size))
-        self._cycles = _cycles
+        for wheel in self.wheels:
+            number_of_cycles = (_lcm // wheel.size) + 1
+            wheel._number_of_cycles = number_of_cycles
+            _positions.extend(wheel.get_positions())
+        self._positions = sorted(_positions, key=lambda p: (p.value, p.wheel.index))
 
     # public properties
+
     @property
     def gear_sizes(self):
-        return self._gear_sizes
+        return [wheel.size for wheel in self.wheels]
 
-    @gear_sizes.setter
-    def gear_sizes(self, value):
-        if self._check_input(value):
-            self._gear_sizes = value
-            self._set_cycles()
-            self._iterator = iter(self.get_rhythm())
+    @property
+    def wheels(self):
+        return self._wheels
 
-    # //public methods
+    @wheels.setter
+    def wheels(self, vals):
+        for val in vals:
+            if not isinstance(val, Wheel):
+                raise TypeError(f'{val} must be of type Wheel not {type(val)} ')
+        self._wheels = []
+        for index, wheel in enumerate(vals):
+            wheel.index = index
+            self._wheels.append(wheel)
+        self._set_positions()
 
     # get
-    def get_cycles(self):
-        return self._cycles
+    def get_positions(self):
+        return self._positions
 
-    def get_result(self):
-        result = set(self._cycles[0])
-        for cycle in self._cycles:
-            result = result.union(set(cycle))
-        result = list(result)
-        result.sort()
-        return list(result)
+    def get_position_values(self):
+        position_values = [p.value for p in self.get_positions()]
+        return list(dict.fromkeys(position_values))
 
     def get_rhythm(self):
-        return xToD(self.get_result())
-
-    # other
-    def __next__(self):
-        return self._iterator.__next__()
-
-    def __iter__(self):
-        return self
+        return xToD(self.get_position_values())
