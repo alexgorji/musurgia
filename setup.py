38f794156830baaef2d357813ce73f6576427387
Alex Gorji <aligorji@hotmail.com>
2020-04-27 17:05:24 +0200

    release 1.10.11 (setup without musicscore in install_requirements)

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ import setuptools
 
 setuptools.setup(
     name="musurgia",
-    version="1.10.10",
+    version="1.10.11",
     author="Alex Gorji",
     author_email="aligorji@hotmail.com",
     description="tools for algorithmic composition",
     url="https://github.com/alexgorji/musurgia.git",
     packages=setuptools.find_packages(),
     install_requires=['quicktions',
-                      'musicscore',
                       'prettytable',
                       'fpdf2',
                       'diff-pdf-visually'
