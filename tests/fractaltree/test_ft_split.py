7c0f2d19b42660bb2007f8ba004243178c419af5
Alex Gorji <aligorji@hotmail.com>
2020-06-14 22:29:09 +0200

    wip

--- tests/fractaltree/test_ft_split.py
+++ tests/fractaltree/test_ft_split.py
@@ -21,3 +21,19 @@ class TestFtSplit(TestCase):
         actual = [x.value for x in s]
         expected = [child.value/3, child.value/3, child.value/3]
         self.assertEqual(expected, actual)
+
+    def test_child_is_not_leaf(self):
+        t = FractalTree(value=10)
+        t.add_layer()
+        child = t.get_children()[0]
+        child.split(1, 1, 1)
+        self.assertFalse(child.is_leaf)
+
+    def test_split_is_leaf(self):
+        t = FractalTree(value=10)
+        t.add_layer()
+        child = t.get_children()[0]
+        s = child.split(1, 1, 1)
+        actual = [x.is_leaf for x in s]
+        expected = [True, True, True]
+        self.assertEqual(expected, actual)
