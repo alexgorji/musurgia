bbad39d956ae880b0b8e0580e117f33ef4aa1038
Alex Gorji <aligorji@hotmail.com>
2020-11-04 19:07:28 +0100

    modified:   tests/agrandom/test_agrandom.py

--- tests/agrandom/test_agrandom.py
+++ tests/agrandom/test_agrandom.py
@@ -23,3 +23,18 @@ class Test(TestCase):
         output = [r.__next__() for i in range(20)]
         expected = [6, 6, 3, 2, 6, 6, 1, 2, 5, 3, 1, 4, 4, 1, 1, 3, 2, 4, 5, 4]
         self.assertEqual(expected, output)
+
+    def test_4(self):
+        pool = [1, 3, 2, 4, 5, 6]
+        r = Random(pool=pool, periodicity=3, seed=30)
+        output = [r.__next__() for i in range(20)]
+        expected = [5, 2, 1, 6, 3, 2, 1, 4, 6, 3, 1, 4, 5, 3, 1, 4, 6, 2, 5, 1]
+        self.assertEqual(expected, output)
+
+    def test_5(self):
+        pool = [1, 3, 2, 4, 5, 6]
+        r1 = Random(pool=pool, periodicity=3, seed=30)
+        r2 = Random(pool=pool, periodicity=5, seed=30)
+        output = [r1.__next__() for i in range(20)]
+        expected = [5, 2, 1, 6, 3, 2, 1, 4, 6, 3, 1, 4, 5, 3, 1, 4, 6, 2, 5, 1]
+        self.assertEqual(expected, output)
