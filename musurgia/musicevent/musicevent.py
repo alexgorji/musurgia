4f165955795d7f079133cf2458d7af31fd7d83b6
alexgorji <aligorji@hotmail.com>
2024-06-21 17:12:33 +0200

    musicevent idea still not clear enough

--- musurgia/musicevent/musicevent.py
+++ musurgia/musicevent/musicevent.py
@@ -7,14 +7,14 @@ from musurgia.musurgia_types import check_type
 
 
 def calculate_duration(quarter_duration: QuarterDuration, tempo: Metronome) -> Fraction:
-    return 60 / (tempo.per_minute / tempo.beat_unit) * quarter_duration
+    return Fraction(60 / (tempo.per_minute / tempo.beat_unit) * quarter_duration)
 
 
 class MusicEvent(Chord):
     _ATTRIBUTES = Chord._ATTRIBUTES.union({'tempo'})
 
-    def __init__(self, quarter_duration: QuarterDuration, tempo: Metronome = Metronome(60), *args: Any, **kwargs: Any):
-        super().__init__(quarter_duration=quarter_duration, *args, **kwargs)
+    def __init__(self, quarter_duration: QuarterDuration, tempo: Metronome = Metronome(60), *args: Any, **kwargs: Any):  # type: ignore[no-untyped-call]
+        super().__init__(quarter_duration=quarter_duration, *args, **kwargs)  # type: ignore
         self._tempo: Metronome
         self.tempo = tempo
 
@@ -23,7 +23,7 @@ class MusicEvent(Chord):
         return self._tempo
 
     @tempo.setter
-    def tempo(self, val: Metronome):
+    def tempo(self, val: Metronome) -> None:
         check_type(val, Metronome, class_name=self.__class__.__name__, property_name='tempo')
         self._tempo = val
 
