097e5207371317ecb188fc99bc98446cebb9f26d
alexgorji <aligorji@hotmail.com>
2024-12-15 19:47:37 +0100

    chore: split Duration into ReadonlyDuration and Duration

--- musurgia/timing/duration.py
+++ musurgia/timing/duration.py
@@ -15,8 +15,7 @@ def _convert_other(other: Union['Duration', ConvertibleToFraction]) -> Fraction:
 
     return Fraction(other)
 
-
-class Duration:
+class ReadonlyDuration:
     def __init__(self, seconds: ConvertibleToFraction = 0, minutes: ConvertibleToFraction = 0, hours: ConvertibleToFraction = 0,
                  *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
@@ -27,61 +26,41 @@ class Duration:
         self._set_clock(hours=hours, minutes=minutes, seconds=seconds)
 
     def _set_clock(self, hours: ConvertibleToFraction, minutes: ConvertibleToFraction, seconds: ConvertibleToFraction) -> None:
-        self.add_seconds(seconds)
-        self.add_minutes(minutes)
-        self.add_hours(hours)
-        self.clock = Clock.convert_seconds_to_clock(
+        self._add_seconds(seconds)
+        self._add_minutes(minutes)
+        self._add_hours(hours)
+        self._clock = Clock.convert_seconds_to_clock(
             Clock.convert_clock_to_seconds(hours, minutes, seconds))
+        self._hours, self._minutes, self._seconds = [Fraction(time) for time in self._clock.get_values()]
+        
+    def _add_seconds(self, val: ConvertibleToFraction) -> None:
+        check_type(val, 'ConvertibleToFraction', class_name=self.__class__.__name__, property_name='seconds')
+        self._seconds += Fraction(val)
+
+    def _add_hours(self, val: ConvertibleToFraction) -> None:
+        check_type(val, 'ConvertibleToFraction', class_name=self.__class__.__name__, property_name='hours')
+        self._hours += Fraction(val)
+
+    def _add_minutes(self, val: ConvertibleToFraction) -> None:
+        check_type(val, 'ConvertibleToFraction', class_name=self.__class__.__name__, property_name='minutes')
+        self._minutes += Fraction(val)
 
     @property
     def clock(self) -> Clock:
         return self._clock
 
-    @clock.setter
-    def clock(self, val: Clock) -> None:
-        check_type(val, Clock, class_name=self.__class__.__name__, property_name='clock')
-        self._clock = val
-        self._hours, self._minutes, self._seconds = [Fraction(time) for time in self.clock.get_values()]
-
-    def add_seconds(self, val: ConvertibleToFraction) -> None:
-        check_type(val, 'ConvertibleToFraction', class_name=self.__class__.__name__, property_name='seconds')
-        self._seconds += Fraction(val)
-
-    def add_hours(self, val: ConvertibleToFraction) -> None:
-        check_type(val, 'ConvertibleToFraction', class_name=self.__class__.__name__, property_name='hours')
-        self._hours += Fraction(val)
-
-    def add_minutes(self, val: ConvertibleToFraction) -> None:
-        check_type(val, 'ConvertibleToFraction', class_name=self.__class__.__name__, property_name='minutes')
-        self._minutes += Fraction(val)
-
+    @property
+    def minutes(self) -> Fraction:
+        return self._minutes
+    
     @property
     def seconds(self) -> Fraction:
         return self._seconds
 
-    @seconds.setter
-    def seconds(self, val: ConvertibleToFraction) -> None:
-        check_type(val, 'ConvertibleToFraction', class_name=self.__class__.__name__, property_name='seconds')
-        self._set_clock(hours=self._hours, minutes=self._minutes, seconds=val)
-
-    @property
-    def minutes(self) -> Fraction:
-        return self._minutes
-
-    @minutes.setter
-    def minutes(self, val: ConvertibleToFraction) -> None:
-        check_type(val, 'ConvertibleToFraction', class_name=self.__class__.__name__, property_name='minutes')
-        self._set_clock(self._hours, val, self._seconds)
-
     @property
     def hours(self) -> Fraction:
         return self._hours
-
-    @hours.setter
-    def hours(self, val: ConvertibleToFraction) -> None:
-        check_type(val, 'ConvertibleToFraction', class_name=self.__class__.__name__, property_name='hours')
-        self._set_clock(val, self._minutes, self._seconds)
-
+    
     def calculate_in_seconds(self) -> Fraction:
         return self.seconds + (60 * self.minutes) + (3600 * self.hours)
 
@@ -170,6 +149,40 @@ class Duration:
 
     def __str__(self) -> str:
         return f"Duration: {self.clock.get_as_string()}"
+
+class Duration(ReadonlyDuration):
+    @ReadonlyDuration.clock.setter
+    def clock(self, val: Clock) -> None:
+        check_type(val, Clock, class_name=self.__class__.__name__, property_name='clock')
+        self._clock = val
+        self._hours, self._minutes, self._seconds = [Fraction(time) for time in self.clock.get_values()]
+
+    def add_seconds(self, val: ConvertibleToFraction) -> None:
+        super()._add_seconds()
+
+    def add_hours(self, val: ConvertibleToFraction) -> None:
+        super()._add_hours()
+
+    def add_minutes(self, val: ConvertibleToFraction) -> None:
+        super()._add_minutes()
+
+    @ReadonlyDuration.seconds.setter
+    def seconds(self, val: ConvertibleToFraction) -> None:
+        check_type(val, 'ConvertibleToFraction', class_name=self.__class__.__name__, property_name='seconds')
+        self._set_clock(hours=self._hours, minutes=self._minutes, seconds=val)
+
+
+    @ReadonlyDuration.minutes.setter
+    def minutes(self, val: ConvertibleToFraction) -> None:
+        check_type(val, 'ConvertibleToFraction', class_name=self.__class__.__name__, property_name='minutes')
+        self._set_clock(self._hours, val, self._seconds)
+
+    @ReadonlyDuration.hours.setter
+    def hours(self, val: ConvertibleToFraction) -> None:
+        check_type(val, 'ConvertibleToFraction', class_name=self.__class__.__name__, property_name='hours')
+        self._set_clock(val, self._minutes, self._seconds)
+
+
 
 def convert_duration_to_quarter_duration_value(metronome: Union[Metronome, int], duration: Union[Duration, float, int, Fraction]) -> Fraction:
     if isinstance(duration, Duration):
