352c1108ea8419ec1edbf54424eb8fc0fb039e95
alexgorji <aligorji@hotmail.com>
2024-12-17 15:23:45 +0100

    feature: add chord property with AttributeError to AbstractChordFactory

--- musurgia/chordfactory.py
+++ musurgia/chordfactory.py
@@ -17,6 +17,18 @@ class AbstractChordFactory(ABC, metaclass=ChordFactoryType):
         super().__init__(*args, **kwargs)
         self._chord: Chord = Chord(60, 1)
 
+    def _update_chord(self) -> None:
+        for method_name in self.__class__._CHORD_UPDATE_METHODS:
+            getattr(self, method_name)()
+
+    @property
+    def chord(self) -> None:
+        raise AttributeError("Use get_chord() instead.")
+
+    @chord.setter
+    def chord(self, value: Any) -> None:
+        raise AttributeError("ChordFactory.chord cannot be set. ")
+
     @abstractmethod
     def update_chord_quarter_duration(self) -> None:
         pass
@@ -24,10 +36,6 @@ class AbstractChordFactory(ABC, metaclass=ChordFactoryType):
     @abstractmethod
     def update_chord_midis(self) -> None:
         pass
-
-    def _update_chord(self) -> None:
-        for method_name in self.__class__._CHORD_UPDATE_METHODS:
-            getattr(self, method_name)()
 
     def get_chord(self) -> "Chord":
         self._update_chord()
