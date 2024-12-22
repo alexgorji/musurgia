32e0418890454f53b09e408bf35202062ec66f9e
alexgorji <aligorji@hotmail.com>
2024-12-22 22:47:34 +0100

    feature: dToX and xToD in util

--- musurgia/utils.py
+++ musurgia/utils.py
@@ -1,4 +1,27 @@
-from typing import Any
+from fractions import Fraction
+from typing import Any, Iterator, Literal, Optional, Sequence
+
+from musurgia.musurgia_types import ConvertibleToFraction, convert_to_fraction
+from musurgia.quantize import get_quantized_positions
+
+
+def dToX(
+    input_list: Sequence[ConvertibleToFraction],
+    first_element: ConvertibleToFraction = 0,
+) -> list[Fraction]:
+    input = convert_to_fraction_list(input_list)
+    output = [convert_to_fraction(first_element)]
+    for i in range(len(input)):
+        output.append(input[i] + output[i])
+    return output
+
+
+def xToD(input_list: Sequence[ConvertibleToFraction]) -> list[Fraction]:
+    input = convert_to_fraction_list(input_list)
+    result = []
+    for i in range(1, len(input)):
+        result.append(input[i] - input[i - 1])
+    return result
 
 
 def flatten(input: list[Any]) -> list[Any]:
