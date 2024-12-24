9d46922e7f893a032c7980ae9f94e5189b0e2213
alexgorji <aligorji@hotmail.com>
2024-12-24 13:44:57 +0100

    test: musical tree with random midis

--- musurgia/tests/trees/musicaltree/test_musical_tree.py
+++ musurgia/tests/trees/musicaltree/test_musical_tree.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+
+from musicscore.midi import Midi
+from musurgia.magicrandom import Random
 from musurgia.tests.utils_for_tests import XMLTestCase, test_fractal_structur_list
 from musurgia.trees.musicaltree import MidiMusicalTree
 
@@ -25,3 +28,29 @@ class TestSimpleMusicalTree(XMLTestCase):
         with self.file_path(path, "simple") as xml_path:
             score.export_xml(xml_path)
 
+
+class TestRandomMusicalTree(XMLTestCase):
+    def setUp(self):
+        self.mt = MidiMusicalTree.create_tree_from_list(
+            test_fractal_structur_list, "duration"
+        )
+        self.mt.get_chord_factory().show_metronome = True
+
+    def set_random_midis(self, musical_tree, root_midi_range, periodicitiy, seed):
+        min_midi, max_midi = root_midi_range
+        random_ = Random(
+            pool=list(range(min_midi, max_midi + 1)),
+            periodicity=periodicitiy,
+            seed=seed,
+        )
+        for node in musical_tree.traverse():
+            node.get_chord_factory().midis = [Midi(next(random_))]
+
+    def test_random_midis(self):
+        self.set_random_midis(
+            musical_tree=self.mt, root_midi_range=(60, 84), seed=10, periodicitiy=7
+        )
+        score = self.mt.export_score()
+        score.get_quantized = True
+        with self.file_path(path, "random") as xml_path:
+            score.export_xml(xml_path)
