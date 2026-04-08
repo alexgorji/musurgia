994baed17146dcae33330c7f61149df41141769c
Alex Gorji <ag@roko.li>
2026-04-08 21:22:53 +0200

    feat(grpahics): pagintor

---
+++ musurgia/graphics/svg/paginator.py
@@ -0,0 +1,188 @@
+import copy
+from decimal import Decimal
+from typing import Any, Mapping
+
+import svg
+
+from musurgia.graphics.container import Container
+from musurgia.graphics.drawobject import DrawObject
+from musurgia.graphics.geometry import Paddings, Position, Scalar, Size
+from musurgia.graphics.page import PageLayout
+from musurgia.graphics.svg.convertors import SVGConverterRegistry
+
+
+def convert_drawobjects_to_svg_group(
+    positioned_drawobjects: list[tuple[Position, DrawObject]],
+) -> svg.G:
+    g = svg.G()
+    g.elements = []
+    for p, d in positioned_drawobjects:
+        g.elements.extend(SVGConverterRegistry.convert(p, d))
+    return g
+
+
+def create_page_rows(
+    page: "SVGPage",
+    number_of_rows: int = 1,
+    options: dict[int, Mapping[str, Any]] | None = None,
+) -> list["SVGPageRow"]:
+    if page.get_rows():
+        raise AttributeError("Page has already rows.")
+    row_height = Decimal(page.get_layout().get_effective_size().height / number_of_rows)
+    for i in range(number_of_rows):
+        row_options = options.get(i + 1) if options else None
+        paddings = Paddings()
+        if options:
+            if row_options := options.get(i + 1):
+                _paddings = row_options.get("paddings")
+                if isinstance(_paddings, Paddings):
+                    paddings = _paddings
+
+        row = SVGPageRow(height=row_height, paddings=paddings)
+        page.add_row(row)
+
+    return page.get_rows()
+
+
+def get_row_content_positions(page: "SVGPage") -> list[Position]:
+    positions = []
+    t, _, _, l = page.get_layout().margins.to_values()
+    y = t
+    for row in page.get_rows():
+        positions.append(
+            Position(l + row.get_paddings().left, y + row.get_paddings().top)
+        )
+        y += row.get_height()
+
+    return positions
+
+
+class SVGPageRow:
+    def __init__(
+        self,
+        height: Scalar,
+        paddings: Paddings = Paddings(),
+    ) -> None:
+        self._height = height
+        self._paddings = paddings
+        self._svg_group: svg.G | None = None
+
+    def get_height(self) -> Scalar:
+        return self._height
+
+    def get_paddings(self) -> Paddings:
+        return self._paddings
+
+    def add_svg_group(self, svg_group: svg.G) -> None:
+        self._svg_group = svg_group
+
+    def get_svg_group(self) -> svg.G | None:
+        return self._svg_group
+
+
+class SVGPage:
+    def __init__(self, layout: PageLayout = PageLayout()) -> None:
+        self._layout = layout
+        self._rows: list[SVGPageRow] = []
+
+    def get_layout(self) -> PageLayout:
+        return self._layout
+
+    def add_row(self, row: SVGPageRow) -> None:
+        self._rows.append(row)
+
+    def add_grid(self) -> None:
+        raise NotImplementedError()
+
+    def get_rows(self) -> list[SVGPageRow]:
+        return self._rows
+
+    def get_row_effective_size(self, row_number: int) -> Size:
+        row = self.get_rows()[row_number - 1]
+        t, r, b, l = row.get_paddings().to_values()
+        return Size(
+            self._layout.get_effective_size().width - r - l, row.get_height() - t - b
+        )
+
+    def copy(self) -> "SVGPage":
+        return SVGPage(layout=copy.deepcopy(self.get_layout()))
+
+    def as_svg(self) -> svg.SVG:
+        w, h = self.get_layout().get_size().to_values()
+        svg_object = svg.SVG(
+            width=svg.Length(w, "mm"),
+            height=svg.Length(h, "mm"),
+            viewBox=svg.ViewBoxSpec(0, 0, w, h),
+        )
+        row_positions = get_row_content_positions(self)
+        svg_object.elements = []
+
+        for position, row in zip(row_positions, self.get_rows()):
+            if group := row.get_svg_group():
+                x, y = position.to_values()
+                wrapper = svg.G(
+                    transform=[svg.Translate(x=float(x), y=float(y))],
+                    elements=[group],
+                )
+                svg_object.elements.append(wrapper)
+        return svg_object
+
+
+class SVGPaginator:
+    def __init__(self, pages: list[SVGPage] = [SVGPage()]) -> None:
+        self._pages = pages
+
+    @staticmethod
+    def make_clip_path(size: Size, clip_id: str) -> svg.ClipPath:
+        return svg.ClipPath(
+            id=clip_id,
+            elements=[svg.Rect(x=0, y=0, width=size.width, height=size.height)],
+        )
+
+    @staticmethod
+    def get_clipped_svg_group(
+        svg_group: svg.G, offset: Scalar, size: Size, clip_id: str = "clip"
+    ) -> svg.G:
+        clip_path = SVGPaginator.make_clip_path(size=size, clip_id=clip_id)
+        return svg.G(
+            clip_path=f"url(#{clip_id})",
+            elements=[
+                svg.Defs(elements=[clip_path]),
+                svg.G(
+                    transform=[svg.Translate(x=float(-offset), y=0)],
+                    elements=[copy.deepcopy(svg_group)],
+                ),
+            ],
+        )
+
+    def paginate(self, container: Container) -> list[SVGPage]:
+        page_number = 1
+        offset = Decimal(0)
+        svg_group = convert_drawobjects_to_svg_group(
+            container.get_draw_objects(positioned=True)
+        )
+        pages = []
+        while offset < container.size.width:
+            page_index = page_number - 1 if page_number <= len(self._pages) else -1
+            try:
+                page = self._pages[page_index]
+            except IndexError:
+                page = self._pages[page_index].copy()
+            rows = page.get_rows()
+            if not rows:
+                rows = create_page_rows(page)
+            id = 1
+            for i, row in enumerate(rows):
+                if offset > container.size.width:
+                    break
+                row_effective_size = page.get_row_effective_size(i + 1)
+                row.add_svg_group(
+                    SVGPaginator.get_clipped_svg_group(
+                        svg_group, offset, row_effective_size, f"id{id}"
+                    )
+                )
+                id += 1
+                offset += row_effective_size.width
+            pages.append(page)
+            page_number += 1
+        return pages
---
+++ musurgia/tests/graphics/svg/paginator/test_paginator.py
@@ -0,0 +1,169 @@
+from decimal import Decimal
+from pathlib import Path
+from unittest import TestCase
+import svg
+
+from musurgia.graphics.geometry import (
+    LineOrientation,
+    Paddings,
+    Margins,
+    Position,
+    Size,
+)
+from musurgia.graphics.page import PageLayout
+from musurgia.graphics.ruler import Ruler
+from musurgia.graphics.svg.paginator import (
+    SVGPage,
+    SVGPageRow,
+    SVGPaginator,
+    convert_drawobjects_to_svg_group,
+    create_page_rows,
+    get_row_content_positions,
+)
+from musurgia.tests.helpers.svg import SVG, SVGTestCase
+
+this_path = Path(__file__)
+
+
+class SVGPaginatorTestCase(TestCase):
+    def test_svg_page_with_one_row(self):
+        page = SVGPage()
+        page_size = page.get_layout().get_size()
+        row = SVGPageRow(
+            height=page_size.height,
+            paddings=Paddings(10, 20, 30, 40),
+        )
+        page.add_row(row)
+        assert page.get_rows() == [row]
+        t, r, b, l = row.get_paddings().to_values()
+        assert page.get_row_effective_size(1) == Size(
+            page_size.width - r - l, row.get_height() - t - b
+        )
+
+    def test_svg_page_create_rows(self):
+        page = SVGPage()
+        rows = create_page_rows(
+            page,
+            number_of_rows=3,
+            options={
+                1: {"paddings": Paddings(10, 20, 30, 40)},
+                2: {"paddings": Paddings(10, 20, 30, 40)},
+                3: {"paddings": Paddings(10, 20, 30, 40)},
+            },
+        )
+        assert page.get_rows() == rows
+        assert len(page.get_rows()) == 3
+
+    def test_svg_page_multiple_rows(self):
+        page = SVGPage(layout=PageLayout(margins=Margins(20, 10, 10, 10)))
+        create_page_rows(
+            page,
+            number_of_rows=3,
+            options={
+                1: {"paddings": Paddings(10, 20, 30, 40)},
+                2: {"paddings": Paddings(10, 20, 30, 40)},
+                3: {"paddings": Paddings(10, 20, 30, 40)},
+            },
+        )
+        row_height = Decimal(page.get_layout().get_effective_size().height / 3)
+
+        for i, row in enumerate(page.get_rows()):
+            assert row.get_height() == row_height
+            t, r, b, l = row.get_paddings().to_values()
+            assert page.get_row_effective_size(i + 1) == Size(
+                page.get_layout().get_effective_size().width - r - l,
+                row.get_height() - t - b,
+            )
+        assert (
+            sum([r.get_height() for r in page.get_rows()])
+            == page.get_layout().get_effective_size().height
+        )
+
+    def test_get_row_content_positions(self):
+        page = SVGPage(layout=PageLayout(margins=Margins(20, 10, 10, 10)))
+        row_height = Decimal(page.get_layout().get_effective_size().height / 3)
+
+        rows = [
+            SVGPageRow(height=row_height, paddings=Paddings(0, 10, 10, 10))
+            for _ in range(3)
+        ]
+        for row in rows:
+            page.add_row(row)
+
+        assert get_row_content_positions(page) == [
+            Position(20, 20),
+            Position(20, 20 + row_height),
+            Position(20, 20 + row_height * 2),
+        ]
+
+    def test_make_clip_path(self):
+        clip_path = SVGPaginator.make_clip_path(Size(100, 200), "clip_path")
+        assert (
+            clip_path.as_str()
+            == '<clipPath id="clip_path"><rect x="0" y="0" width="100" height="200"/></clipPath>'
+        )
+
+    def test_get_clipped_svg_group(self):
+        ruler = Ruler(type=LineOrientation.HORIZONTAL, length=1000)
+
+        clipped_ruler_svg_group = SVGPaginator.get_clipped_svg_group(
+            svg_group=convert_drawobjects_to_svg_group([(Position(0, 0), ruler)]),
+            offset=100,
+            size=Size(200, ruler.size.height),
+        )
+        assert isinstance(clipped_ruler_svg_group, svg.G)
+
+    def test_paginate(self):
+        ruler = Ruler(type=LineOrientation.HORIZONTAL, length=4 * 205)
+        paginator = SVGPaginator()
+        pages = paginator.paginate(ruler)
+        assert len(pages) == 4
+        for p in pages:
+            assert isinstance(p, SVGPage)
+            assert len(p.get_rows()) == 1
+            assert isinstance(p.get_rows()[0].get_svg_group(), svg.G)
+
+    def test_paginate_multiple_rows(self):
+        ruler = Ruler(type=LineOrientation.HORIZONTAL, length=1000)
+        page = SVGPage(layout=PageLayout(margins=Margins(20, 10, 10, 10)))
+        create_page_rows(
+            page,
+            number_of_rows=3,
+            options={
+                1: {"paddings": Paddings(0, 10, 10, 10)},
+                2: {"paddings": Paddings(0, 10, 10, 10)},
+                3: {"paddings": Paddings(0, 10, 10, 10)},
+            },
+        )
+        paginator = SVGPaginator([page])
+        pages = paginator.paginate(ruler)
+        assert len(pages) == 2
+
+
+class SVGPaginatorAsSVG(SVGTestCase):
+    def test_page_as_svg_group(self):
+        ruler = Ruler(
+            type=LineOrientation.HORIZONTAL,
+            length=500,
+            color="blue",
+            options={
+                "label": {"color": "green"},
+            },
+        )
+        page = SVGPage(layout=PageLayout(margins=Margins(20, 10, 10, 10)))
+        create_page_rows(
+            page,
+            number_of_rows=4,
+            options={
+                1: {"paddings": Paddings(0, 10, 10, 10)},
+                2: {"paddings": Paddings(0, 10, 10, 10)},
+                3: {"paddings": Paddings(0, 10, 10, 10)},
+                4: {"paddings": Paddings(0, 10, 10, 10)},
+            },
+        )
+        paginator = SVGPaginator([page])
+        page = paginator.paginate(ruler)[0]
+        assert isinstance(page.as_svg(), svg.SVG)
+        svg_path = SVG(page.as_svg().as_str()).write_to_path(
+            self.create_test_path(this_path, "", "svg")
+        )
